5eaabf17bf1cdb6792fc1541b249bad298134f7b
kirahan <hanzhaoapply@gmail.com>
2016-03-02 16:42:19 +0800

    change interface

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,8 @@ var popup ={
 		//初始化
 		popup.styleInit()
 		//监听保存
+
+		$('.default-setting').on('click',popup.Default())
 		$('.tts-save').on('click',function(event){
 			chrome.storage.local.get('ttsoption',function(items){
 				items.ttsoption.volume=parseFloat($("#volume").val())
@@ -28,6 +30,50 @@ var popup ={
 				popup.styleInit()
 			})
 		})
+		$('.timer-save').on('click',function(options){
+			options.timer_display = $('#timer-display').is(':checked')
+			options.timer__auto_danmu = $('#timer-auto-danmu').is(':checked')
+			chrome.storage.local.set({'timer_option':options},function(){
+				popup.HtmlAlert("更新完成")
+				popup.styleInit()
+			})
+		})
+		$('.reply-save').on('click',function(options){
+			options.reply = $('#auto-reply-switch').is(':checked')
+			options.min_interval = $('#min-reply-interval').val()
+			options.scramble = $('#reply-scramble-response-allowed').is(':checked')
+			options.competitor = $('#reply-competitor-info-allowed').is(':checked')
+			options.wcaitem = $('#reply-wca-query-allowed').is(':checked')
+			options.manager = $('#manager-no-limited').is(':checked')
+			chrome.storage.local.set({'reply_option':options},function(){
+				popup.HtmlAlert("更新完成")
+				popup.styleInit()
+			})
+		})
+		$('.result-save').on('click',function(options){
+			options.monitor = $('#result-monitor').is(':checked')
+			options.upload = $('#result-upload').is(':checked')
+			chrome.storage.local.set({'result_option':options},function(){
+				popup.HtmlAlert("更新完成")
+				popup.styleInit()
+			})
+		})
+		$('.voting-save').on('click',function(options){
+			options = 1
+			chrome.storage.local.set({'voting_option':options},function(){
+				popup.HtmlAlert("更新完成")
+				popup.styleInit()
+			})
+		})
+		$('.lucky-save').on('click',function(options){
+			options.awardname = $('#lucky-award-name').val()
+			options.manuallist = $('#lucky-manual-list').val()
+			chrome.storage.local.set({'lucky_option':options},function(){
+				popup.HtmlAlert("更新完成")
+				popup.styleInit()
+			})
+		})
+
 		$('.doubanfm').on('click',function(){
 			chrome.storage.local.set({'doubanfm':1})
 			chrome.windows.create({url:"http://douban.fm/partner/uc",width:550,height:325,type:'panel'},function(window){
@@ -41,12 +87,6 @@ var popup ={
 			$("#volume").val(items.ttsoption.volume)
 			$("#rate").val(items.ttsoption.rate)
 			$("#pitch").val(items.ttsoption.pitch)
-			var volume=items.ttsoption.volume*100
-			var rate=items.ttsoption.rate*10
-			var pitch=items.ttsoption.pitch*50
-			$('.tts-volume .progress-bar').css('width',volume+'%')
-			$('.tts-rate .progress-bar').css('width',rate+'%')
-			$('.tts-pitch .progress-bar').css('width',pitch+'%')
 		});
 		chrome.storage.local.get('sendmsg',function(items){
 			$("#send-msg").val(items.sendmsg)
@@ -54,12 +94,48 @@ var popup ={
 		chrome.storage.local.get('wellcomemsg',function(items){
 			$("#wellcome-msg").val(items.wellcomemsg)
 		});
+		chrome.storage.local.get('timer_option',function(options){
+			$('#timer-display').prop('checked',options.timer_display)
+			$('#timer-auto-danmu').prop('checked',options.timer__auto_danmu)
+		});
+		chrome.storage.local.get('reply_option',function(options){
+			$('#auto-reply-switch').prop('checked',options.reply)
+			$('#min-reply-interval').val(options.min_interval)
+			$('#reply-scramble-response-allowed').prop('checked',options.scramble)
+			$('#reply-competitor-info-allowed').prop('checked',options.competitor)
+		  	$('#reply-wca-query-allowed').prop('checked',options.wcaitem)
+			$('#manager-no-limited').prop('checked',options.manager)
+		});
+		chrome.storage.local.get('result_option',function(options){
+			$('#result-monitor').prop('checked',options.monitor)
+			$('#result-upload').prop('checked',options.upload)
+		});
+		chrome.storage.local.get('voting_option',function(options){
+
+		});
+		chrome.storage.local.get('lucky_option',function(options){
+
+		});
 	},
 	HtmlAlert:function(message,msgcat){
 		var msgcat = arguments[0]?arguments[0]:'success';
 		$('#htmlalert').text(message);
 		$('#htmlalert').addClass('alert-'+msgcat).show();
 		$('#htmlalert').fadeOut(5000);
+	},
+	Default:function(){
+		$('#timer-display').prop('checked',true)
+		$('#timer-auto-danmu').prop('checked',false)
+		$('#auto-reply-switch').prop('checked',false)
+		$('#min-reply-interval').val(1)
+		$('#reply-scramble-response-allowed').prop('checked',true)
+		$('#reply-competitor-info-allowed').prop('checked',true)
+		$('#reply-wca-query-allowed').prop('checked',true)
+		$('#manager-no-limited').prop('checked',true)
+		$('#result-monitor').prop('checked',false)
+		$('#result-upload').prop('checked',false)
+		$('#lucky-award-name').val('')
+		$('#lucky-manual-list').val('')
 	}
 }
 //setInterval(walkloops, 2000);
